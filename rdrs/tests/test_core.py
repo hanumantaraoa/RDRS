ef945d6fcf6905b45332ed5ff3ff0fefd8f95df0
hanumantaraoa <hanumantaraoa@gmail.com>
2026-09-21 23:32:10 +0530

    Update test_core.py

--- rdrs/tests/test_core.py
+++ rdrs/tests/test_core.py
@@ -1,7 +1,7 @@
 import os
 import pytest
-from .rdrs.core.entropy import calculate_shannon_entropy
-from .rdrs.core.config import load_config
+from /rdrs/core/entropy import calculate_shannon_entropy
+from /rdrs/core/config import load_config
 
 def test_entropy_of_zero_file(tmp_path):
     empty_file = tmp_path / "empty.dat"
