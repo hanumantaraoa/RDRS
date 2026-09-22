c6e661c916058f612b5cdfb9f0367cfd9be8291b
hanumantaraoa <hanumantaraoa@gmail.com>
2026-09-22 13:48:48 +0530

    Update test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,7 @@
 import os
 import pytest
-from .app.core.entropy.py import calculate_shannon_entropy
-from .app.core.config.py import load_config
+from "app/core/entropy.py" import calculate_shannon_entropy
+from "app/core/config.py" import load_config
 
 def test_entropy_of_zero_file(tmp_path):
     empty_file = tmp_path / "empty.dat"
